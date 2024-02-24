fe2fcb213c136935f0c36c5d1e398621911ead3f
Oluchukwu Joseph <devjoseph016@gmail.com>
2024-02-24 06:37:26 -0800

    Modified files to pass betty standards

--- sort.h
+++ sort.h
@@ -11,9 +11,9 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
@@ -22,6 +22,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void swap(int* i, int* j);
+void swap(int *i, int *j);
 
 #endif
